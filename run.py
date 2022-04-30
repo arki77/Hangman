d6d5d029c6e5965f52c461c1ba13f1a5faa87584
arki <mrarkinpl@gmail.com>
2022-04-30 09:56:55 +0200

    #2 - Random Slogan

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-import os, json
-
-slowo = 'test test'
-
-
-usedLetter = []
-info = ''
-heart = 10
+import os, json, random
 
 def readJson(filename):
 	with open(filename) as json_file:
@@ -24,35 +17,53 @@ def printDesing(list, words):
 				text += '_ '
 	return text
 
+def randomSlogan(data):
+	categories = []
+	for x in data:
+		categories.append(x)
+	category = random.choice(categories)
+	return random.choice(data[category])
+
+
+data = readJson(r'wisielec\base.json')
+
+
+usedLetter = []
+info = ''
+heart = 10
+guessWord = randomSlogan(data).upper()
+
 while True:
-	data = readJson(r'wisielec\base.json')
-	print(data)
-	# os.system('cls')
-	design = printDesing(usedLetter, slowo)
+	os.system('cls')
+	design = printDesing(usedLetter, guessWord)
 	info_used = f'Used letters: {usedLetter}\n'
 	print(info_used)
 	print(design)
 	print('')
 	print(info)
 
-
-	keyadd = input('Type Letter: ')
-	usedLetter.append(keyadd)
-	design = printDesing(usedLetter, slowo)
-	if not design.count('_') == 0: 
-		print(design.count('_'))
-		if heart != 0:
-			if keyadd in slowo:
-				info = f'Great! The letter `{keyadd}` is in the word!\n'
-				continue
+	keyadd = input('Type Letter: ').upper()
+	
+	if len(keyadd) == 1:
+		
+		usedLetter.append(keyadd)
+		design = printDesing(usedLetter, guessWord)
+		if not design.count('_') == 0: 
+			print(design.count('_'))
+			if heart != 0:
+				if keyadd in guessWord:
+					info = f'Great! The letter `{keyadd}` is in the word!\n'
+					continue
+				else:
+					heart -= 1
+					info = f'The letter `{keyadd}` is not in the word. Numbers of attempts left: {heart} '
+					print(f'Number of attempts left: {heart}')
 			else:
-				heart -= 1
-				info = f'The letter `{keyadd}` is not in the word. Numbers of attempts left: {heart} '
-				print(f'Number of attempts left: {heart}')
+				print(f'You lose!\nGuessed word is `{guessWord}`')
+				break
 		else:
-			print(f'You lose!\nGuessed word is `{slowo}`')
+			print(f'You win!\nGuessed word is `{guessWord}`')
 			break
 	else:
-		print(f'You win!\nGuessed word is `{slowo}`')
-		break
+		info = 'You can use one letter!'
 

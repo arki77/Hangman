b2d39fe53771f3a92b587c0c4474c4c293a1976e
arki <mrarkinpl@gmail.com>
2022-04-30 09:34:07 +0200

    Update #1

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
+import os, json
+
 slowo = 'test test'
 
-new = ''
-for x in slowo:
-	if x == " ":
-		new += "  "
-	else:
-		new += "_ "
-
-print(new)
 
 usedLetter = []
+info = ''
+heart = 10
+
+def readJson(filename):
+	with open(filename) as json_file:
+		data = json.load(json_file)
+	return data
 
 def printDesing(list, words):
 	text = ''
@@ -24,7 +25,34 @@ def printDesing(list, words):
 	return text
 
 while True:
-	keyadd = input('Letter?\n')
+	data = readJson(r'wisielec\base.json')
+	print(data)
+	# os.system('cls')
+	design = printDesing(usedLetter, slowo)
+	info_used = f'Used letters: {usedLetter}\n'
+	print(info_used)
+	print(design)
+	print('')
+	print(info)
+
+
+	keyadd = input('Type Letter: ')
 	usedLetter.append(keyadd)
 	design = printDesing(usedLetter, slowo)
-	print(design)
+	if not design.count('_') == 0: 
+		print(design.count('_'))
+		if heart != 0:
+			if keyadd in slowo:
+				info = f'Great! The letter `{keyadd}` is in the word!\n'
+				continue
+			else:
+				heart -= 1
+				info = f'The letter `{keyadd}` is not in the word. Numbers of attempts left: {heart} '
+				print(f'Number of attempts left: {heart}')
+		else:
+			print(f'You lose!\nGuessed word is `{slowo}`')
+			break
+	else:
+		print(f'You win!\nGuessed word is `{slowo}`')
+		break
+
